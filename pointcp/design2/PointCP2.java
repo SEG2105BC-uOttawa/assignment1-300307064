9710419a7871fbb1fdcf47f33ebe9053d4f41ccc
ngkhanggg <nguyenkhangchu2004@gmail.com>
2023-10-04 13:29:52 -0400

    convert to pointcp5's subclass

--- pointcp/design2/PointCP2.java
+++ pointcp/design2/PointCP2.java
@@ -1,77 +1,31 @@
 package design2;
 
+import design5.PointCP5;
+
 /**
  * Polar coordinates
  */
 
-public class PointCP2 {
-    private char typeCoord;
-    private double rho;
-    private double theta;
-
-    public PointCP2(char typeCoord, double rho, double theta) {
-        if (typeCoord != 'C' || typeCoord != 'P') throw new IllegalArgumentException();
-        
-        this.rho = rho;
-        this.theta = theta;
-        this.typeCoord = typeCoord;
-    }
-
-    public double getX() {
-        return (typeCoord == 'C') ? rho : (Math.cos(Math.toRadians(theta)) * rho);
-      }
-    
-      public double getY() {
-        return (typeCoord == 'C') ? theta : (Math.sin(Math.toRadians(theta)) * rho);
-      }
-    
-      public double getRho() {
-        return (typeCoord == 'P') ? rho : (Math.sqrt(Math.pow(rho, 2) + Math.pow(theta, 2)));
-      }
-    
-      public double getTheta() {
-        return (typeCoord == 'P') ? theta : (Math.toDegrees(Math.atan2(theta, rho)));
-      }
-
-    public void convertStorageToCartesian() {
-        if (typeCoord != 'C') {
-            double temp = getX();
-            theta = getY();
-            rho = temp;
-      
-            typeCoord = 'C';
-          }
-    }
-
-    public void convertStorageToPolar() {
-        if (typeCoord != 'P') {
-            double temp = getRho();
-            theta = getTheta();
-            rho = temp;
-      
-            typeCoord = 'P';
-          }
-    }
-
-    public double getDistance(PointCP2 pointB) {
-        double deltaX = getX() - pointB.getX();
-        double deltaY = getY() - pointB.getY();
-
-        return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
+public class PointCP2 extends PointCP5 {
+    public PointCP2(double rho, double theta) {
+        super('P', rho, theta);
     }
 
     public PointCP2 rotatePoint(double rotation) {
         double radRotation = Math.toRadians(rotation);
         double X = getX();
         double Y = getY();
+
+        double newX = (Math.cos(radRotation) * X) - (Math.sin(radRotation) * Y);
+        double newY = (Math.sin(radRotation) * X) + (Math.cos(radRotation) * Y);
+
+        double newXToRho = Math.sqrt(Math.pow(newX, 2) + Math.pow(newY, 2));
+        double newYToTheta = Math.toDegrees(Math.atan2(newY, newX));
     
-        return new PointCP2('P',
-          (Math.cos(radRotation) * X) - (Math.sin(radRotation) * Y),
-          (Math.sin(radRotation) * X) + (Math.cos(radRotation) * Y)
-        );
+        return new PointCP2(newXToRho, newYToTheta);
     }
 
     public String toString() {
-        return "Stored as " + (typeCoord == 'C' ? "Cartesian (" + getX() + ", " + getY() + ")" : "Polar [" + getRho() + ", " + getTheta() + "]") + "\n";
+        return "Stored as " + (getTypeCoord() == 'C' ? "Cartesian (" + getX() + ", " + getY() + ")" : "Polar [" + getRho() + ", " + getTheta() + "]") + "\n";
     }
 }
--- pointcp/design3/PointCP3.java
+++ pointcp/design3/PointCP3.java
@@ -1,63 +1,14 @@
 package design3;
 
+import design5.PointCP5;
+
 /**
  * Polar coordinates
  */
 
-public class PointCP3 {
-    private char typeCoord;
-    private double x;
-    private double y;
-
-    public PointCP3(char typeCoord, double x, double y) {
-        if (typeCoord != 'C' || typeCoord != 'P') throw new IllegalArgumentException();
-        
-        this.x = x;
-        this.y = y;
-        this.typeCoord = typeCoord;
-    }
-
-    public double getX() {
-        return (typeCoord == 'C') ? x : (Math.cos(Math.toRadians(y)) * x);
-      }
-    
-      public double getY() {
-        return (typeCoord == 'C') ? y : (Math.sin(Math.toRadians(y)) * x);
-      }
-    
-      public double getRho() {
-        return (typeCoord == 'P') ? x : (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
-      }
-    
-      public double getTheta() {
-        return (typeCoord == 'P') ? y : (Math.toDegrees(Math.atan2(y, x)));
-      }
-
-    public void convertStorageToCartesian() {
-        if (typeCoord != 'C') {
-            double temp = getX();
-            y = getY();
-            x = temp;
-      
-            typeCoord = 'C';
-          }
-    }
-
-    public void convertStorageToPolar() {
-        if (typeCoord != 'P') {
-            double temp = getRho();
-            y = getTheta();
-            x = temp;
-      
-            typeCoord = 'P';
-          }
-    }
-
-    public double getDistance(PointCP3 pointB) {
-        double deltaX = getX() - pointB.getX();
-        double deltaY = getY() - pointB.getY();
-
-        return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
+public class PointCP3 extends PointCP5 {
+    public PointCP3(double x, double y) {        
+        super('C', x, y);
     }
 
     public PointCP3 rotatePoint(double rotation) {
@@ -65,13 +16,13 @@ public class PointCP3 {
         double X = getX();
         double Y = getY();
     
-        return new PointCP3('C',
+        return new PointCP3(
           (Math.cos(radRotation) * X) - (Math.sin(radRotation) * Y),
           (Math.sin(radRotation) * X) + (Math.cos(radRotation) * Y)
         );
     }
 
     public String toString() {
-        return "Stored as " + (typeCoord == 'C' ? "Cartesian  (" + getX() + ", " + getY() + ")" : "Polar [" + getRho() + ", " + getTheta() + "]") + "\n";
+        return "Stored as " + (getTypeCoord() == 'C' ? "Cartesian  (" + getX() + ", " + getY() + ")" : "Polar [" + getRho() + ", " + getTheta() + "]") + "\n";
     }
 }
