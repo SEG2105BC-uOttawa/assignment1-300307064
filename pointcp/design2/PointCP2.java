ad4dc4872e525aedf0160115b34f81a1cf1c8ed6
ngkhanggg <nguyenkhangchu2004@gmail.com>
2023-10-04 11:56:10 -0400

    fix bugs

--- pointcp/design2/PointCP2.java
+++ pointcp/design2/PointCP2.java
@@ -1,62 +1,75 @@
 package design2;
 
-import design1.PointCP;
-
 /**
  * Polar coordinates
  */
 
 public class PointCP2 {
+    private char typeCoord;
     private double rho;
     private double theta;
 
     public PointCP2(double rho, double theta) {
         this.rho = rho;
         this.theta = theta;
-    }
-
-    public double getRho() {
-        return rho;
-    }
-
-    public double getTheta() {
-        return theta;
+        typeCoord = 'P';
     }
 
     public double getX() {
-        return Math.cos(Math.toRadians(theta)) * rho;
-    }
-
-    public double getY() {
-        return Math.sin(Math.toRadians(theta)) * rho;
-    }
+        return (typeCoord == 'C') ? rho : (Math.cos(Math.toRadians(theta)) * rho);
+      }
+    
+      public double getY() {
+        return (typeCoord == 'C') ? theta : (Math.sin(Math.toRadians(theta)) * rho);
+      }
+    
+      public double getRho() {
+        return (typeCoord == 'P') ? rho : (Math.sqrt(Math.pow(rho, 2) + Math.pow(theta, 2)));
+      }
+    
+      public double getTheta() {
+        return (typeCoord == 'P') ? theta : (Math.toDegrees(Math.atan2(theta, rho)));
+      }
 
     public void convertStorageToCartesian() {
-        double temp = getX();
-        theta = getY();
-        rho = temp;
+        if (typeCoord != 'C') {
+            double temp = getX();
+            theta = getY();
+            rho = temp;
+      
+            typeCoord = 'C';
+          }
     }
 
-    public double getDistance(PointCP pointB) {
+    public void convertStorageToPolar() {
+        if (typeCoord != 'P') {
+            double temp = getRho();
+            theta = getTheta();
+            rho = temp;
+      
+            typeCoord = 'P';
+          }
+    }
+
+    public double getDistance(PointCP2 pointB) {
         double deltaX = getX() - pointB.getX();
         double deltaY = getY() - pointB.getY();
 
         return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
     }
 
-    public PointCP rotatePoint(double rotation) {
+    public PointCP2 rotatePoint(double rotation) {
         double radRotation = Math.toRadians(rotation);
         double X = getX();
         double Y = getY();
     
-        return new PointCP(
-          'C',
+        return new PointCP2(
           (Math.cos(radRotation) * X) - (Math.sin(radRotation) * Y),
           (Math.sin(radRotation) * X) + (Math.cos(radRotation) * Y)
         );
     }
 
     public String toString() {
-        return "Stored as " + "Polar [" + getRho() + ", " + getTheta() + "]" + "\n";
+        return "Stored as " + (typeCoord == 'C' ? "Cartesian  (" + getX() + ", " + getY() + ")" : "Polar [" + getRho() + ", " + getTheta() + "]") + "\n";
     }
 }
