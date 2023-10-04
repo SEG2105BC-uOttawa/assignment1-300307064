c8c2412763f6eaface85d53d43b431bce5d2e355
ngkhanggg <nguyenkhangchu2004@gmail.com>
2023-10-04 11:56:25 -0400

    add test file

---
+++ pointcp/design2/PointCP2Test.java
@@ -0,0 +1,78 @@
+package design2;
+
+import java.io.*;
+
+public class PointCP2Test {
+    public static void main(String[] args) {
+        PointCP2 point;
+
+        System.out.println("(Desgin 2) Cartesian-Polar Coordinates Conversion Program");
+
+        try {
+            point = new PointCP2(
+                Double.valueOf(args[0]).doubleValue(),
+                Double.valueOf(args[1]).doubleValue()
+            );
+        }
+        catch (Exception e) {
+            if (args.length != 0) {
+                System.out.println("Invalid arguments on command line");
+            }
+
+            try {
+                point = getInput();
+            }
+            catch (IOException ex) {
+                System.out.println("Error getting input. Ending program.");
+                return;
+            }
+        }
+
+        System.out.println("\nYou entered:\n" + point);
+        point.convertStorageToCartesian();
+        System.out.println("\nAfter asking to store as Cartesian:\n" + point);
+        point.convertStorageToPolar();
+        System.out.println("\nAfter asking to store as Polar:\n" + point);
+    }
+
+    private static PointCP2 getInput() throws IOException {
+        byte[] buffer = new byte[1024];
+        boolean isOk = false;
+        String theInput = "";
+
+        double a = 0.0;
+        double b = 0.0;
+
+        for (int i = 0; i < 2; i++) {
+            while (!isOk) {
+                isOk = true;
+
+                System.out.print("Enter the value of " + (i == 0 ? "Rho" : "Theta") + " using a decimal point(.): ");
+
+                for (int k = 0; k < 1024; k++) {
+                    buffer[k] = '\u0020';
+                }
+
+                System.in.read(buffer);
+                theInput = new String(buffer).trim();
+
+                try {
+                    if (i == 0) {
+                        a = Double.valueOf(theInput).doubleValue();
+                    }
+                    else {
+                        b = Double.valueOf(theInput).doubleValue();
+                    }
+                }
+                catch (Exception e) {
+                    System.out.println("Incorrect input");
+                    isOk = false;
+                }
+            }
+
+            isOk = false;
+        }
+
+        return (new PointCP2(a, b));
+    }
+}
